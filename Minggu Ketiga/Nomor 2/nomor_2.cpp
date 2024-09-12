2f4d952aa665f2f934499f4897261ba4ff674af7
revysatyagunawan <gunawanrevysatya@gmail.com>
2024-09-12 20:38:49 +0700

    Add files via upload

---
+++ Minggu Ketiga/Nomor 2/nomor_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cout << "akan ditentukan bilangan yang terbesar dari 3 bilangan berbeda a, b, dan c" << endl;
+	cout << "masukkan nilai a, b, dan c" << endl;
+	
+	double a, b, c;
+	cin >> a >> b >> c;
+	
+	double terbesar;
+	if (a > b) {
+		if (c > a) {
+			terbesar = c;
+		} else {
+			terbesar = a;
+		} 	
+	} else {
+		if (c > b) {
+			terbesar = c;
+		} else {
+			terbesar = b;
+		}
+	}
+	
+	cout << "bilangan yang terbesar adalah " << terbesar << endl;
+	
+	return 0;
+	
+}
